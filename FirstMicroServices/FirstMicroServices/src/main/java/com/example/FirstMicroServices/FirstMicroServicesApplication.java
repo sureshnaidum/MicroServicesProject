8e4e996bcdaa32d038118c5982d3c81bee7ea80d
suresh <naidu@gmail.com>
2019-11-24 11:02:40 +0530

    adding project into remote repository

---
+++ FirstMicroServices/FirstMicroServices/src/main/java/com/example/FirstMicroServices/FirstMicroServicesApplication.java
@@ -0,0 +1,13 @@
+package com.example.FirstMicroServices;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FirstMicroServicesApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FirstMicroServicesApplication.class, args);
+	}
+
+}
---
+++ FirstMicroServices/FirstMicroServices/src/test/java/com/example/FirstMicroServices/FirstMicroServicesApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.FirstMicroServices;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FirstMicroServicesApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
